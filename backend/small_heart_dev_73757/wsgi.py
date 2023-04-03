c801df1acae11d6f47102f4b636a424c45dd5454
Crowdbotics <engineering@crowdbotics.com>
2023-04-03 01:08:29 +0000

    Initial commit.

---
+++ backend/small_heart_dev_73757/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for small_heart_dev_73757 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'small_heart_dev_73757.settings')
+
+application = get_wsgi_application()
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "small-heart-dev-73757",
+  url: "https://small-heart-dev-73757.botics.co",
+  api: "https://small-heart-dev-73757.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
